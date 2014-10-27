8e3b395c056a211e7bb13ce792d2f35f9f83240e
karthick lakshman <karthick.lakshman@gmail.com>
2014-10-27 13:31:42 +0530

    Create binder.js

---
+++ binder.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+
+setInterval(function(){
+  if ($('html').attr('class')=='shiny-busy') {
+    setTimeout(function() {
+      if ($('html').attr('class')=='shiny-busy') {
+        $('div.busy').show()
+      }
+    }, 100)
+  } else {
+    $('div.busy').hide()
+  }
+}, 100)
+
+});
